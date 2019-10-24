a9bf852eb81416b1592d268edbc02b117c0aceb4
edpugantir <56380814+edpugantir@users.noreply.github.com>
2019-10-24 13:36:01 +1100

    Delete insert.sql

--- insert.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO `user` VALUES 
-	(1,'ravi','edpuganti','ravie@xyz.com',TRUE),
-	(2,'vasu','uppu','vasuu@xyz.com', TRUE),
-	(3,'anvesh','vaka','anveshv@xyz.com', TRUE),
-	(4,'anand','agarwal','ananda@xyz.com', TRUE),
-	(5,'taran','adarsh','tarana@xyz.com', TRUE);
-	(6,'ajit','rao','ajitr@xyz.com', TRUE);
-	(7,'anoop','bhat','anoopb@xyz.com', TRUE);
-        (8,'jatin', 'anand','jatina@xyz.com', TRUE) 
